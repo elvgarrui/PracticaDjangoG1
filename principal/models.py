d48b01e476b442c0d5f1abf146d9ae44e3064908
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2017-12-01 08:46:25 +0100

    Modelos iniciales

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Banco(models.Model):
+    entidadId = models.CharField(max_length=4)
+    nombre = models.CharField(max_length=150)
+    def __unicode__(self):
+        return self.nombre
+    
+class Sucursales(models.Model):
+    sucursalId = models.CharField(max_length=4)
+    banco = models.ForeignKey(Banco)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=12)
+    def __unicode__(self):
+        return self.sucursalId
+
+class Usuario(models.Model):
+    usuario = models.ForeignKey(User)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.nombre
+
+class Cuenta(models.Model):
+    numero = models.CharField(max_length=20)
+    usuario = models.ForeignKey(Usuario)
+    saldo = models.FloatField()
+    def __unicode__(self):
+        return self.numero
+    
